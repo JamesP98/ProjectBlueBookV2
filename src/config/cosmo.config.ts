96e52a6b618ef5a5f6f063200646b7c08b168aa8
JamesP98 <james.plimbley@googlemail.com>
2019-08-18 19:41:09 +0100

    Initial commit

---
+++ src/config/cosmo.config.ts
@@ -0,0 +1,9 @@
+export const config = {
+    production: false,
+    read_key: 'dHZeBdFCCrXhTDdyhsr1os6Zypgrn4lygEVLcda1cCXaIvwKbJ',
+    write_key: 'd5k9zS4f659w6Ka4wdU770FJ3RLQpbnrE8R2VBe7ggBIV5pUFh',
+    bucket_id: '5d59993d12acfb2b8b094c05',
+    bucket_slug: 'projectbluebook', 
+    URL: 'https://api.cosmicjs.com/v1/',
+    };
+    
